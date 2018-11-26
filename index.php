8cccc4d7e1fef43c47e1cdace7838dc68c53d3f9
elmerpino <jpino@ucol.mx>
2018-11-26 19:02:55 +0000

    Correccion de errores #4

--- index.php
+++ index.php
@@ -16,9 +16,6 @@
 
     <!-- MetisMenu CSS -->
     <link href="css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/sb-admin-2.css" rel="stylesheet">
  
 
 </head>
@@ -52,16 +49,5 @@ require "cfg/conexion.php";
 ?>
    <button type="button" class="btn btn-success" onclick="window.location.href='sala_remota.html'">Agregar Sala</button>
    </div>
-    <!-- jQuery -->
-    <script src="../js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../js/plugins/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../.js/sb-admin-2.js"></script>
 
 </body>
