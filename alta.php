9361b035ce6d75bdc1ecbf3d3c282c87beaebdd8
elmerpino <jpino@ucol.mx>
2018-11-26 18:29:13 +0000

    Ya agregaga salas #2

--- alta.php
+++ alta.php
@@ -1,4 +1,5 @@
 <?php session_start(); 
+
 //datos para establecer la conexion con la base de mysql.
 require "cfg/conexion.php";
 
@@ -11,16 +12,15 @@ if (isset($_GET["nom"])) {
     $ip = $_GET["ip"];
     $isdn = $_GET["isdn"];
     
-    
-    
     // Hay campos en blanco
     if($name==NULL|$responsable==NULL|$telefono==NULL|$email==NULL|$ip==NULL|$isdn==NULL) {
         echo "un campo est&aacute; vacio.";
         formRegistro();
     }else{
 
-                $query = 'INSERT INTO SALA_REMOTA (nombre, telefono, email_responsable, ip, issdn) VALUES ("'.$name.'","'.$responsable.'","'.$telefono.'","'.$email.'","'.$ip.'","'.$isdn.'")';
-                mysql_query($query) or die(mysql_error());
+                $query = 'INSERT INTO SALA_REMOTA (nombre, responsable , telefono, email_responsable, ip, isdn) VALUES ("'.$name.'","'.$responsable.'","'.$telefono.'","'.$email.'","'.$ip.'","'.$isdn.'")';
+                
+                mysqli_query($conexion, $query) or die(mysqli_error($conexion));
                 echo 'La sala '.$name.' ha sido registrada de manera satisfactoria.<br/>';
    header('Location: index.php');
             }
